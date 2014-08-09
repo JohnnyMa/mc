07721e9f58eea5164f810478230324685a528604
jma <jma>
2014-08-09 17:59:46 +0800

    remove useless theme, add i18n

--- protected/messages/zh_cn/Common.php
+++ protected/messages/zh_cn/Common.php
@@ -2,22 +2,26 @@
 
 return array(
 
-	//system menu
-	'login'=>'aa',
-	'nav_customer_mgmt'=>'客户信息管理',
-	'nav_loan_mgmt'=>'贷款业务管理',
-	'nav_after_loan_mgmt'=>'贷后管理',
-	'nav_daily_mgmt'=>'日终处理',
-	'nav_report_mgmt'=>'报表管理',
-	'nav_system_mgmt'=>'系统管理',
+    //system menu
+    'login' => 'aa', 
+    'nav_customer_mgmt' => '客户信息管理', 
+    'nav_loan_mgmt' => '贷款业务管理', 
+    'nav_after_loan_mgmt' => '贷后管理', 
+    'nav_daily_mgmt' => '日终处理', 
+    'nav_report_mgmt' => '报表管理', 
+    'nav_system_mgmt' => '系统管理', 
+    
+    //login form
+    'auto_login_next_time' => '下次自动登录 ', 
+    'user_login' => '用户登录',
+    'username' => '用户名',
+    'password' => '密码',
+    'login' => '登录',
 
-
-	'nav_customer_mgmt'=>'客户信息管理',
-	'nav_customer_mgmt'=>'客户信息管理',
-	'nav_customer_mgmt'=>'客户信息管理',
-	'nav_customer_mgmt'=>'客户信息管理',
-	'nav_customer_mgmt'=>'客户信息管理',
-	'nav_customer_mgmt'=>'客户信息管理',
-
-
+    'nav_customer_mgmt' => '客户信息管理', 
+    'nav_customer_mgmt' => '客户信息管理', 
+    'nav_customer_mgmt' => '客户信息管理', 
+    'nav_customer_mgmt' => '客户信息管理', 
+    'nav_customer_mgmt' => '客户信息管理', 
+    'nav_customer_mgmt' => '客户信息管理', 
 );
--- themes/abound/views/layouts/tpl_navigation.php
+++ themes/abound/views/layouts/tpl_navigation.php
@@ -1,61 +1,43 @@
 <div class="navbar navbar-inverse navbar-fixed-top">
-	<div class="navbar-inner">
-    <div class="container">
-        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-     
-          <!-- Be sure to leave the brand out there if you want it shown -->
-          <a class="brand" href="#">jma</a>
-          
-          <div class="nav-collapse">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
 
-			<?php $this->widget('zii.widgets.CMenu',array(
-                    'htmlOptions'=>array('class'=>'pull-right nav'),
-                    'submenuHtmlOptions'=>array('class'=>'dropdown-menu'),
-					'itemCssClass'=>'item-test',
-                    'encodeLabel'=>false,
-                    'items'=>array(
-                        /*
-                        array('label'=>'Graphs & Charts', 'url'=>array('/site/page', 'view'=>'graphs'), 'visible'=>!Yii::app()->user->isGuest),
-                        array('label'=>'Forms', 'url'=>array('/site/page', 'view'=>'forms'), 'visible'=>!Yii::app()->user->isGuest),
-                        array('label'=>'Concat', 'url'=>array('/site/contact'), 'visible'=>!Yii::app()->user->isGuest),
-                        array('label'=>'Tables', 'url'=>array('/site/page', 'view'=>'tables'), 'visible'=>!Yii::app()->user->isGuest),
-						array('label'=>'Interface', 'url'=>array('/site/page', 'view'=>'interface'), 'visible'=>!Yii::app()->user->isGuest),
-                        array('label'=>'Typography', 'url'=>array('/site/page', 'view'=>'typography'), 'visible'=>!Yii::app()->user->isGuest),
-                        */
+            <!-- Be sure to leave the brand out there if you want it shown -->
+            <a class="brand" href="#">jma</a>
 
-                        array('label'=>Yii::t('nav_customer_mgmt', 'aa'), 'url'=>array('/customer/index'), 'visible'=>!Yii::app()->user->isGuest),
-                        array('label'=>'Loan Mgmt', 'url'=>array('/loan/index'), 'visible'=>!Yii::app()->user->isGuest),
-                        array('label'=>'Task Mgmt', 'url'=>array('/task/index'), 'visible'=>!Yii::app()->user->isGuest),
-                        array('label'=>'Report Mgmt', 'url'=>array('/report/index'), 'visible'=>!Yii::app()->user->isGuest),
-                        array('label'=>'Admin Mgmt', 'url'=>array('/admin/view'), 'visible'=>!Yii::app()->user->isGuest),
+            <div class="nav-collapse">
 
-                        /*array('label'=>'Gii generated', 'url'=>array('customer/index')),*/
-                        array('label'=>'My Account <span class="caret"></span>', 'url'=>'#','itemOptions'=>array('class'=>'dropdown','tabindex'=>"-1"),'linkOptions'=>array('class'=>'dropdown-toggle','data-toggle'=>"dropdown"), 
-                        'items'=>array(
-                            array('label'=>'My Messages <span class="badge badge-warning pull-right">26</span>', 'url'=>'#'),
-							array('label'=>'My Tasks <span class="badge badge-important pull-right">112</span>', 'url'=>'#'),
-							array('label'=>'My Invoices <span class="badge badge-info pull-right">12</span>', 'url'=>'#'),
-							array('label'=>'Separated link', 'url'=>'#'),
-							array('label'=>'One more separated link', 'url'=>'#'),
-                        ), 'visible'=>!Yii::app()->user->isGuest),
-                        array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-                        array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
-                    ),
-                )); ?>
-    	</div>
+                <?php $this -> widget('zii.widgets.CMenu', array('htmlOptions' => array('class' => 'pull-right nav'), 'submenuHtmlOptions' => array('class' => 'dropdown-menu'), 'itemCssClass' => 'item-test', 'encodeLabel' => false, 'items' => array(
+    /*
+     array('label'=>'Graphs & Charts', 'url'=>array('/site/page', 'view'=>'graphs'), 'visible'=>!Yii::app()->user->isGuest),
+     array('label'=>'Forms', 'url'=>array('/site/page', 'view'=>'forms'), 'visible'=>!Yii::app()->user->isGuest),
+     array('label'=>'Concat', 'url'=>array('/site/contact'), 'visible'=>!Yii::app()->user->isGuest),
+     array('label'=>'Tables', 'url'=>array('/site/page', 'view'=>'tables'), 'visible'=>!Yii::app()->user->isGuest),
+     array('label'=>'Interface', 'url'=>array('/site/page', 'view'=>'interface'), 'visible'=>!Yii::app()->user->isGuest),
+     array('label'=>'Typography', 'url'=>array('/site/page', 'view'=>'typography'), 'visible'=>!Yii::app()->user->isGuest),
+     */
+
+    array('label' => Yii::t('Common', 'nav_customer_mgmt'), 'url' => array('/customer/index'), 'visible' => !Yii::app() -> user -> isGuest), 
+    array('label' => Yii::t('Common', 'nav_loan_mgmt'), 'url' => array('/loan/index'), 'visible' => !Yii::app() -> user -> isGuest), 
+    array('label' => Yii::t('Common', 'nav_daily_mgmt'), 'url' => array('/task/index'), 'visible' => !Yii::app() -> user -> isGuest), 
+    array('label' => Yii::t('Common', 'nav_report_mgmt'), 'url' => array('/report/index'), 'visible' => !Yii::app() -> user -> isGuest), 
+    array('label' => Yii::t('Common', 'nav_customer_mgmt'), 'url' => array('/admin/view'), 'visible' => !Yii::app() -> user -> isGuest),
+    array('label' => Yii::t('Common', 'nav_system_mgmt'), 'url' => array('/admin/view'), 'visible' => Yii::app() -> user -> isGuest),
+
+    /*array('label'=>'Gii generated', 'url'=>array('customer/index')),*/
+    array('label' => 'My Account <span class="caret"></span>', 'url' => '#', 'itemOptions' => array('class' => 'dropdown', 'tabindex' => "-1"), 'linkOptions' => array('class' => 'dropdown-toggle', 'data-toggle' => "dropdown"), 'items' => array( array('label' => 'My Messages <span class="badge badge-warning pull-right">26</span>', 'url' => '#'), array('label' => 'My Tasks <span class="badge badge-important pull-right">112</span>', 'url' => '#'), array('label' => 'My Invoices <span class="badge badge-info pull-right">12</span>', 'url' => '#'), array('label' => 'Separated link', 'url' => '#'), array('label' => 'One more separated link', 'url' => '#'), ), 'visible' => !Yii::app() -> user -> isGuest), array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app() -> user -> isGuest), array('label' => 'Logout (' . Yii::app() -> user -> name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app() -> user -> isGuest), ), ));
+                ?>
+            </div>
+        </div>
     </div>
-	</div>
 </div>
 
 <div class="subnav navbar navbar-fixed-top">
     <div class="navbar-inner">
-    	<div class="container">
-        
-        	<div class="style-switcher pull-left">
+        <div class="container">
+
+            <div class="style-switcher pull-left">
                 <a href="javascript:chooseStyle('none', 60)" checked="checked"><span class="style" style="background-color:#0088CC;"></span></a>
                 <a href="javascript:chooseStyle('style2', 60)"><span class="style" style="background-color:#7c5706;"></span></a>
                 <a href="javascript:chooseStyle('style3', 60)"><span class="style" style="background-color:#468847;"></span></a>
@@ -63,12 +45,12 @@
                 <a href="javascript:chooseStyle('style5', 60)"><span class="style" style="background-color:#d85515;"></span></a>
                 <a href="javascript:chooseStyle('style6', 60)"><span class="style" style="background-color:#a00a69;"></span></a>
                 <a href="javascript:chooseStyle('style7', 60)"><span class="style" style="background-color:#a30c22;"></span></a>
-          	</div>
-           <form class="navbar-search pull-right" action="">
-           	 
-           <input type="text" class="search-query span2" placeholder="Search">
-           
-           </form>
-    	</div><!-- container -->
+            </div>
+            <form class="navbar-search pull-right" action="">
+
+                <input type="text" class="search-query span2" placeholder="Search">
+
+            </form>
+        </div><!-- container -->
     </div><!-- navbar-inner -->
 </div><!-- subnav -->
